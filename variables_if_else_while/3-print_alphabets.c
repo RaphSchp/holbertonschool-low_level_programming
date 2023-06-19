d06d064d08fc27550381600acde881c8f80cb2cc
RaphSchp <raphaelschopfer@gmail.com>
2023-06-19 07:05:11 -0700

    Task3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Parameter: describe the parameter
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c, b;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (b = 'A'; b <= 'Z'; b++)
+putchar(b);
+putchar('\n');
+return (0);
+}
