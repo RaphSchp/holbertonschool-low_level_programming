a0b5261ee47c9a1d3bfcfb5e49e5d7e59888f8b6
RaphSchp <raphaelschopfer@gmail.com>
2023-06-19 14:44:48 -0700

    Task0''''

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
-* main- Entry point
-* @void: Null value
-* Description: Print _putchar, followed by a new line.
-* Return: Zero value
-*/
-int main(void) 
+ * main - Entry point
+ * @void: Null value
+ * Description: Print _putchar, followed by a new line.
+ * Return: Zero value
+ */
+int main(void)
 {
-printf("_putchar\n");
-return (0);
+    printf("_putchar\n");
+    return (0);
 }
