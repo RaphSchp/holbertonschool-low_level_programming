3df35fd1f306648702181a9d35779e152fc5bab6
RaphSchp <raphaelschopfer@gmail.com>
2023-06-23 05:44:16 -0700

    Task5'

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
 
 void more_numbers(void)
 {
-	int a, j;
+	int i, j;
 
-	for (a = 1; a <= 10; a++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (b >= 10)
+			if (j >= 10)
 				_putchar('1');
-			_putchar (b % 10 + '0');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
