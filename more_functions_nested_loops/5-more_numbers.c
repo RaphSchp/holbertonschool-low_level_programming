38de8078e60a38dd7cf1d2417345cc7f5579dfe6
RaphSchp <raphaelschopfer@gmail.com>
2023-06-23 08:44:00 -0700

    prints 10 times the numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * more_numbers - function that prints 10 times the numbers,
+ * from 0 to 14, followed by a new line.
  * Return: 0
  */
 
