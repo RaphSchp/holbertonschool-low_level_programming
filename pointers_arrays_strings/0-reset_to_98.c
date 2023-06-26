e719ed4235dd9e8ac5d61115154f8529ba42cab2
RaphSchp <raphaelschopfer@gmail.com>
2023-06-26 05:39:06 -0700

    Add main.h & Task 0.98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * 98 Battery st., a function that takes a pointer to an int
+ * as parameter and updates the value it points to to 98.
+ * @n: a value
+ * Return:
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98
+}
