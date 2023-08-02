2b1b1b606e3544cb795d9cfee8008f7b0e46ee6b
RaphSchp <raphaelschopfer@gmail.com>
2023-08-02 08:25:12 +0000

    add 1-list_len.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -9,13 +9,15 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
